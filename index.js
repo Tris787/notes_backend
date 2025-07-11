c7c9c2b1b713ba13ed95ae26505dc42fcd3133c8
Tris787 <thomas_kuemmet@gmx.de>
2025-07-11 10:30:29 +0200

    add frontend

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
